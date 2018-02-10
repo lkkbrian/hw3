be09be73f6f262cd82e0d73d302c7dd43ca76887
lkkbrian <35850909+lkkbrian@users.noreply.github.com>
2018-02-10 01:05:53 -0800

    Update diamond-lines.js

--- diamond-lines.js
+++ diamond-lines.js
@@ -1,4 +1,14 @@
-for (var i = 0; i <= 175; i = i + 10) {
-  line(175, i, 175 + i, i); line(175, i, 175 - i, i);
-  
+for (var i = 0; i <= 170; i = i + 10) {
+     var startX = i;
+     var startY = 170 - i;
+     var endX = 340 - i;
+     var endY = 170 - i;
+     line(startX, startY, endX, endY);
+}
+for (var i = 0; i <= 170; i = i + 10) {
+     var startX = i;
+     var startY = 170 + i;
+     var endX = 340 - i;
+     var endY = 170 + i;
+     line(startX, startY, endX, endY);
 }
